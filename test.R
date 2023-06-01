0ecca532f8cf819ff1165e51e2a795fdaed93f5e
Ethan McIntosh <84473423+mciethan@users.noreply.github.com>
2023-06-01 16:41:48 -0400

    basic census map

--- test.R
+++ test.R
@@ -1,10 +1,34 @@
 library(dplyr)
 library(shiny)
 library(leaflet)
+library(tidycensus)
 library(RColorBrewer)
 
+census_api_key("3910e99aea0a472b50f5cdc422c9a3395b3c87b3")
+
+#v21 <- load_variables(2021, "acs5/subject", cache = TRUE)
+
+my_states = c("MA")
+my_vars <- c(
+  total_households = "S1901_C01_001"
+  , median_household_income = "S1901_C01_012"
+)
+
+cs <- get_acs(
+  geography = "tract",
+  variables = my_vars,
+  state = my_states,
+  county = "025",
+  year = 2021,
+  survey="acs5",
+  output = "wide",
+  geometry = TRUE,
+  cache_table = TRUE
+)
+df <- cs %>% sf::st_transform(4326)
+
 ui <- fluidPage(
-  titlePanel(h1("Quake Dashboard", align = "center")),
+  titlePanel(h1("Neighborhood Change Dashboard", align = "center")),
   sidebarLayout(
     sidebarPanel(
       sliderInput("range", "Magnitudes", min(quakes$mag), max(quakes$mag),
@@ -25,7 +49,7 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   # Reactive expression for the data subsetted to what the user selected
   filteredData <- reactive({
-    quakes[quakes$mag >= input$range[1] & quakes$mag <= input$range[2],]
+    df
   })
   # TODO: try creating a reactive expression using a hash table: ht <- new.env(hash=TRUE) => ht[[key]] <- df
   # however, we'd need to think about whether to filter or hash if we got to the point of custom time steps
@@ -33,15 +57,29 @@ server <- function(input, output, session) {
   # This reactive expression represents the palette function,
   # which changes as the user makes selections in UI.
   colorpal <- reactive({
-    colorNumeric(input$colors, quakes$mag)
+    colorNumeric(input$colors, df$median_household_incomeE)
   })
   
   output$map <- renderLeaflet({
     # Use leaflet() here, and only include aspects of the map that
     # won't need to change dynamically (at least, not unless the
     # entire map is being torn down and recreated).
-    leaflet(quakes) %>% addTiles() %>%
-      fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
+    # leaflet(quakes, width="100%", height="100%") %>% addTiles() %>%
+    #   fitBounds(~min(long), ~min(lat), ~max(long), ~max(lat))
+    
+    leaflet(df, height = "100%", width = "100%") %>%
+      addProviderTiles(provider = "CartoDB.Positron") %>%
+      addPolygons(
+        stroke = F,
+        smoothFactor = 0,
+        fillOpacity = 0.7)
+        # color = ~ pal(current_data)) %>%
+      # addLegend("bottomright",
+      #   pal = pal,
+      #   values = ~ current_data %>% append(values = c(0, max_val)),
+      #   title = legend_label,
+      #   opacity = 1,
+      #   na.label = 'Tracts with little or no population')
   })
   
   # Incremental changes to the map (in this case, replacing the
@@ -53,14 +91,14 @@ server <- function(input, output, session) {
     
     leafletProxy("map", data = filteredData()) %>%
       clearShapes() %>%
-      addCircles(radius = ~10^mag/10, weight = 1, color = "#777777",
-                 fillColor = ~pal(mag), fillOpacity = 0.7, popup = ~paste(mag)
+      addPolygons(weight = 1, color = "#777777",
+                 fillColor = ~pal(median_household_incomeE), fillOpacity = 0.7 #, popup = ~paste(median_household_incomeE)
       )
   })
   
   # Use a separate observer to recreate the legend as needed.
   observe({
-    proxy <- leafletProxy("map", data = quakes)
+    proxy <- leafletProxy("map", data = df)
     
     # Remove any existing legend, and only if the legend is
     # enabled, create a new one.
@@ -68,7 +106,7 @@ server <- function(input, output, session) {
     if (input$legend) {
       pal <- colorpal()
       proxy %>% addLegend(position = "bottomright",
-                          pal = pal, values = ~mag
+                          pal = pal, values = ~median_household_incomeE
       )
     }
   })
