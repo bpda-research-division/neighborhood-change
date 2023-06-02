8f578190e7a44afc8faafaed6f5fe9de2d30d677
Ethan McIntosh <84473423+mciethan@users.noreply.github.com>
2023-06-02 15:15:03 -0400

    basic functional slider with polygons redrawn

--- test.R
+++ test.R
@@ -14,25 +14,34 @@ my_vars <- c(
   , median_household_income = "S1901_C01_012"
 )
 
-cs <- get_acs(
-  geography = "tract",
-  variables = my_vars,
-  state = my_states,
-  county = "025",
-  year = 2021,
-  survey="acs5",
-  output = "wide",
-  geometry = TRUE,
-  cache_table = TRUE
-)
-df <- cs %>% sf::st_transform(4326)
+years <- c(2010, 2014, 2018)
+
+get_acs_by_yr <- function(yr) {
+  ct <- get_acs(
+    geography = "tract",
+    variables = my_vars,
+    state = my_states,
+    county = "025",
+    year = yr,
+    survey="acs5",
+    output = "wide",
+    geometry = TRUE,
+    cache_table = TRUE
+  )
+  ct$year <- yr
+  ct
+}
+cs <- lapply(years, get_acs_by_yr)
+df <- cs %>% bind_rows() %>% 
+  mutate(median_household_incomeE = ifelse(startsWith(NAME, "Census Tract 98"), NaN,median_household_incomeE)) %>%
+  sf::st_transform(4326)
 
 ui <- fluidPage(
   titlePanel(h1("Neighborhood Change Dashboard", align = "center")),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("range", "Magnitudes", min(quakes$mag), max(quakes$mag),
-                  value = range(quakes$mag), step = 0.1
+      sliderInput("yearSelect", "Drag slider to see change over time", 
+                  2010, 2018, value = 2018, step = 4, sep = "", ticks=FALSE
       ),
       leafletOutput("map")
       , width=6 # will probably go for 6 on the slider + map side...
@@ -49,10 +58,13 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   # Reactive expression for the data subsetted to what the user selected
   filteredData <- reactive({
-    df
+    # quakes[quakes$mag >= input$range[1] & quakes$mag <= input$range[2],]
+    df[df$year == input$yearSelect,]
   })
-  # TODO: try creating a reactive expression using a hash table: ht <- new.env(hash=TRUE) => ht[[key]] <- df
-  # however, we'd need to think about whether to filter or hash if we got to the point of custom time steps
+  # TODO: try creating a reactive expression using a hash table: 
+  # ht <- new.env(hash=TRUE) => ht[[key]] <- df
+  # however, we'd need to think about whether to filter or hash if we got to 
+  # the point of arbitrary time steps for a single variable
   
   # This reactive expression represents the palette function,
   # which changes as the user makes selections in UI.
@@ -105,8 +117,10 @@ server <- function(input, output, session) {
     proxy %>% clearControls()
     if (input$legend) {
       pal <- colorpal()
-      proxy %>% addLegend(position = "bottomright",
-                          pal = pal, values = ~median_household_incomeE
+      proxy %>% addLegend_decreasing(position = "bottomright",
+                          pal = pal, values = ~median_household_incomeE,
+                          na.label = 'Tracts with little or no population',
+                          decreasing = TRUE, title = "Median Household Income ($)"
       )
     }
   })
@@ -178,5 +192,106 @@ server <- function(input, output, session) {
   #   }
   # })
 # }
+
+addLegend_decreasing <- function (map, position = c("topright", "bottomright", "bottomleft","topleft"),
+                                  pal, values, na.label = "NA", bins = 7, colors, 
+                                  opacity = 0.5, labels = NULL, labFormat = labelFormat(), 
+                                  title = NULL, className = "info legend", layerId = NULL, 
+                                  group = NULL, data = getMapData(map), decreasing = FALSE) {
+  
+  position <- match.arg(position)
+  type <- "unknown"
+  na.color <- NULL
+  extra <- NULL
+  if (!missing(pal)) {
+    if (!missing(colors)) 
+      stop("You must provide either 'pal' or 'colors' (not both)")
+    if (missing(title) && inherits(values, "formula")) 
+      title <- deparse(values[[2]])
+    values <- evalFormula(values, data)
+    type <- attr(pal, "colorType", exact = TRUE)
+    args <- attr(pal, "colorArgs", exact = TRUE)
+    na.color <- args$na.color
+    if (!is.null(na.color) && col2rgb(na.color, alpha = TRUE)[[4]] == 
+        0) {
+      na.color <- NULL
+    }
+    if (type != "numeric" && !missing(bins)) 
+      warning("'bins' is ignored because the palette type is not numeric")
+    if (type == "numeric") {
+      cuts <- if (length(bins) == 1) 
+        pretty(values, bins)
+      else bins   
+      if (length(bins) > 2) 
+        if (!all(abs(diff(bins, differences = 2)) <= 
+                 sqrt(.Machine$double.eps))) 
+          stop("The vector of breaks 'bins' must be equally spaced")
+      n <- length(cuts)
+      r <- range(values, na.rm = TRUE)
+      cuts <- cuts[cuts >= r[1] & cuts <= r[2]]
+      n <- length(cuts)
+      p <- (cuts - r[1])/(r[2] - r[1])
+      extra <- list(p_1 = p[1], p_n = p[n])
+      p <- c("", paste0(100 * p, "%"), "")
+      if (decreasing == TRUE){
+        colors <- pal(rev(c(r[1], cuts, r[2])))
+        labels <- rev(labFormat(type = "numeric", cuts))
+      }else{
+        colors <- pal(c(r[1], cuts, r[2]))
+        labels <- rev(labFormat(type = "numeric", cuts))
+      }
+      colors <- paste(colors, p, sep = " ", collapse = ", ")
+    }
+    else if (type == "bin") {
+      cuts <- args$bins
+      n <- length(cuts)
+      mids <- (cuts[-1] + cuts[-n])/2
+      if (decreasing == TRUE){
+        colors <- pal(rev(mids))
+        labels <- rev(labFormat(type = "bin", cuts))
+      }else{
+        colors <- pal(mids)
+        labels <- labFormat(type = "bin", cuts)
+      }
+    }
+    else if (type == "quantile") {
+      p <- args$probs
+      n <- length(p)
+      cuts <- quantile(values, probs = p, na.rm = TRUE)
+      mids <- quantile(values, probs = (p[-1] + p[-n])/2, na.rm = TRUE)
+      if (decreasing == TRUE){
+        colors <- pal(rev(mids))
+        labels <- rev(labFormat(type = "quantile", cuts, p))
+      }else{
+        colors <- pal(mids)
+        labels <- labFormat(type = "quantile", cuts, p)
+      }
+    }
+    else if (type == "factor") {
+      v <- sort(unique(na.omit(values)))
+      colors <- pal(v)
+      labels <- labFormat(type = "factor", v)
+      if (decreasing == TRUE){
+        colors <- pal(rev(v))
+        labels <- rev(labFormat(type = "factor", v))
+      }else{
+        colors <- pal(v)
+        labels <- labFormat(type = "factor", v)
+      }
+    }
+    else stop("Palette function not supported")
+    if (!any(is.na(values))) 
+      na.color <- NULL
+  }
+  else {
+    if (length(colors) != length(labels)) 
+      stop("'colors' and 'labels' must be of the same length")
+  }
+  legend <- list(colors = I(unname(colors)), labels = I(unname(labels)), 
+                 na_color = na.color, na_label = na.label, opacity = opacity, 
+                 position = position, type = type, title = title, extra = extra, 
+                 layerId = layerId, className = className, group = group)
+  invokeMethod(map, data, "addLegend", legend)
+}
 
 shinyApp(ui, server)
