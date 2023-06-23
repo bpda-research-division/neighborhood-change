7f2e3be81efd84c92ef024173dad4299657b1b04
Ethan McIntosh <84473423+mciethan@users.noreply.github.com>
2023-06-23 09:52:25 -0400

    spacing adjustments + added an about page
    fixes #5

--- ui.R
+++ ui.R
@@ -26,7 +26,6 @@ colnames(neigh_vars) <- c("name", "start", "end", "step")
 
 all_vars <- list(tract_vars, neigh_vars)
 names(all_vars) <- c("tracts", "neighborhoods")
-t <- names(all_vars)
 
 geoTabPanelUI <- function(geo_type, variables) {
   ns <- NS(geo_type)
@@ -35,7 +34,7 @@ geoTabPanelUI <- function(geo_type, variables) {
   initial_step <- as.numeric(variables$step[1])
   
   # each variable has a display name, a start year, an end year, and a timestep, 
-  tabPanel(tools::toTitleCase(geo_type),
+  tabPanel(tools::toTitleCase(geo_type), style='padding:10px;',
     sidebarPanel(style = "height: 90vh;",
                  fluidRow(
                    column(width = 6,
@@ -47,11 +46,14 @@ geoTabPanelUI <- function(geo_type, variables) {
                    )
                  ),
                  fluidRow(style='padding:10px;',
-                          column(width=7, align = 'center',
+                          column(width=4, align = 'center',
                                  HTML(sprintf("<b>2. Select one or more %s on the map to update the charts.</b>", geo_type))
                           ), # this may turn into a textOutput and live in the server if we want areas to say tracts/neighborhoods instead
-                          column(width=5, align='left',
+                          column(width=3, align='center',
                                  actionButton(ns("clearSelections"), "Clear all selections")
+                          ),
+                          column(width=5, align='center',
+                                 htmlOutput(ns("selectionText"))
                           )
                           
                           
@@ -60,62 +62,41 @@ geoTabPanelUI <- function(geo_type, variables) {
                    htmlwidgets::prependContent(html_fix),
                  width=6 # will probably go for 6 on the slider + map side...
     ),
-    mainPanel(style='padding:10px;',
+    mainPanel( #style='padding:10px;',
       # selectInput("colors", "Color Scheme",
       #             rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
       # ),
       # checkboxInput("legend", "Show legend", TRUE),
       plotlyOutput(ns("bar_chart")),
-      htmlOutput(ns("selectionText"), style='padding:10px;'),
+      # htmlOutput(ns("selectionText"), style='padding:10px;'),
       plotlyOutput(ns("line_chart")),
       width = 6 # and 6 on the bar + line side
     )
   )
 }
 
-ui <- fluidPage(tags$style(type = "text/css", ".irs-grid-pol.small {height: 0px;}"), # this css hides the minor tick marks on the slider
+aboutTabPanelUI <- function(title) {
+  tabPanel(title, style='padding:10px;',
+           "under construction")
+}
+
+tabGenerator <- function(name) {
+  if (name == "About") {
+    aboutTabPanelUI(name) # can pass in other data if we want a modular about page
+  }
+  else {
+    geoTabPanelUI(name, all_vars[[name]])
+  }
+}
+
+ui <- fluidPage(tags$style(type = "text/css", "#buttons {align-items: center; justify-content: center}
+                           .irs-grid-pol.small {height: 0px;}
+                           "), # this css hides the minor tick marks on the slider
   chooseSliderSkin("Shiny"),
-  headerPanel(h1("Neighborhood Change Dashboard", align = "center")),
+  headerPanel(h1("Boston Neighborhood Change Dashboard", align = "center")),
   do.call(tabsetPanel, 
-          # append(
-            lapply(names(all_vars), function(geo_type) {
-              geoTabPanelUI(geo_type, all_vars[[geo_type]])
-            }), 
-          #   tabPanel("About")
-          # )
+          lapply(append(names(all_vars), "About"), function(name) {
+            tabGenerator(name)
+          })
   )
-  # sidebarPanel(style = "height: 90vh;",
-  #              fluidRow(
-  #                column(width = 6,
-  #                       selectInput("variable", "1. Select data:", choices = c("Income", "Age"))
-  #                ),
-  #                column(width = 6, 
-  #                       sliderInput("yearSelect", "3. Drag the slider to see change over time:",
-  #                                   2010, 2018, value = 2010, step = 2, sep = "", ticks=TRUE)
-  #                )
-  #              ),
-  #              fluidRow(style='padding:10px;',
-  #                column(width=6, align = 'right',
-  #                       HTML("<b>2. Select one or more areas on the map to update the charts.</b>")
-  #                ), # this may turn into a textOutput and live in the server if we want areas to say tracts/neighborhoods instead
-  #                column(width=6, align='center',
-  #                       actionButton("clearSelections", "Clear all selections")
-  #                )
-  # 
-  # 
-  #              ),
-  #              leafletOutput("map", height="80%") %>% 
-  #                htmlwidgets::prependContent(html_fix),
-  #              width=6 # will probably go for 6 on the slider + map side...
-  # ),
-  # mainPanel(
-  #   # selectInput("colors", "Color Scheme",
-  #   #             rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
-  #   # ),
-  #   # checkboxInput("legend", "Show legend", TRUE),
-  #   plotlyOutput("bar_chart"),
-  #   htmlOutput("selectionText", style='padding:10px;'),
-  #   plotlyOutput("line_chart"),
-  #   width = 6 # and 6 on the bar + line side
-  # )
 )
